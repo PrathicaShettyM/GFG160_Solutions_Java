2608643f3a13e5128816da52adb3816ff0337d9c
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-11-20 05:23:02 +0530

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,26 @@
+# rotate an array
+class Solution {
+    static void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n = arr.length;
+        // check overflow of 'd' size
+        d = d%n;
+        
+        // reverse 0 to d elements: 1st 'd' elements
+        reverse(arr, 0, d-1);
+        // reverse d to n-1 elements: 1st n-d elements
+        reverse(arr, d, n-1);
+        // reverse 0 to n-1 elements: reverse the entire array
+        reverse(arr, 0, n-1);
+    }
+}
